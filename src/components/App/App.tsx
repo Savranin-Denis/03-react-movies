a27924f319e2a339e6c26d876cdf54e69352d5e9
Savranin-Denis <savranindenisv@gmail.com>
2026-09-12 23:15:03 +0300

    add request api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,29 @@
+import toast from 'react-hot-toast';
+import SearchBar from '../SearchBar/SearchBar';
+import { useState } from 'react';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+
 export default function App() {
-  return <></>;
+  const [films, setFilms] = useState<Movie[]>([]);
+
+  const handleSearch = async (query: string) => {
+    setFilms([]);
+    try {
+      const response = await fetchMovies(query);
+      if (response.length === 0) {
+        toast.error('No movies found for your request.');
+        return;
+      }
+      setFilms(response);
+    } catch {
+      toast.error('Something went wrong. Please try again later.');
+    }
+  };
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+    </>
+  );
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,3 +1,17 @@
-// import axios from 'axios';
+import axios from 'axios';
+// import type { Movie } from '../types/movie';
+const myKey = import.meta.env.VITE_TMDB_TOKENY;
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
-export function fetchMovies() {}
+export async function fetchMovies(query: string) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      query,
+    },
+    headers: {
+      Authorization: `Bearer ${myKey}`,
+    },
+  });
+  console.log(response.data.results);
+  return response.data.results;
+}
